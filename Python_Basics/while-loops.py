791472dcdbe8224377f73821fc804b991f402341
surajrawat11 <isurajrawat23@gmail.com>
2026-06-19 05:22:08 +0530

    Add new code

--- Python_Basics/while-loops.py
+++ Python_Basics/while-loops.py
@@ -5,9 +5,33 @@ while a<=30:
 
 # Question 1
 # Separate each digit of a number and print it on the new line.
-b = int(input("Tell a number :- "))
-while b > 0:
-    print(b%10)
-    b = b//10
-    
+a = int(input("Tell a number :- "))
+while a > 0:
+    print(a%10)
+    a = a//10
 
+# Question 2
+#  Accept a number and print its reverse
+b = int(input("enter a number :- "))
+rev = 0
+while b>0:
+    rev = rev*10+b%10
+    b = b//10
+
+print(rev)
+
+# Question 3
+# Accept a number and check if it is a pallindromic number 
+# (If number and its reverse are equal)
+
+c = int(input("Enter a number :- "))
+rev = 0
+copy = c
+while a>0:
+    rev = rev*10 + a%10
+    c = c//10
+
+if copy==rev:
+    print("pallindrome number")
+else:
+    print("not pallindrome") 
