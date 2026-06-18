262cccdb7605bb0969c8e6f477531dca375326c4
surajrawat11 <isurajrawat23@gmail.com>
2026-06-19 03:00:03 +0530

    Add new code

--- Python_Basics/string.py
+++ Python_Basics/string.py
@@ -11,13 +11,51 @@ for i in range(len(b)-1,-1,-1):
 # Question 2
 # Check string is Pallindrome or not 
 
-c = "Naman"
+c = "shreyain"
 d =""
-for i in range(len(a)-1,-1,-1):
+for i in range(len(c)-1,-1,-1):
     d = d+c[i]
 
-if d == a:
+if d == c:
     print("This string is pallindrome")
 else:
     print("It's not a pallindrome")
+
+# Question 3 
+"""
+Count all letters, digits, and special symbols from a given
+string
+
+ Given: str1 = "P@#yn26at^&i5ve"
+
+ Expected Outcome:
+
+ Total counts of chars, digits, and symbols
+
+ Chars = 8
+
+ Digits = 3
+
+ Symbol = 4
+
+"""
+
+
+x = "dldjalkj123326%^&*@#@#"
+char = 0
+dig = 0
+spchr = 0
+
+for i in x:
+    if i.isdigit():
+        dig+=1
+    elif i.isalpha():
+        char+=1
+    else:
+        spchar+=1
+
+print(f"your digit are {dig}\n")
+print(f"your characters are {char}\n")
+print(f"your special characters are {spchr}\n")
+
 
---
+++ Python_Basics/while-loops.py
@@ -0,0 +1,13 @@
+a = 1
+while a<=30:
+    print(a)
+    a = a+1
+
+# Question 1
+# Separate each digit of a number and print it on the new line.
+b = int(input("Tell a number :- "))
+while b > 0:
+    print(b%10)
+    b = b//10
+    
+
