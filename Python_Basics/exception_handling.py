25b5223d202b7fcf980219884aaa0c23aae8790e
surajrawat11 <isurajrawat23@gmail.com>
2026-06-22 20:29:36 +0530

    exceptions in python

--- Python_Basics/exception_handling.py
+++ Python_Basics/exception_handling.py
@@ -8,4 +8,35 @@ if a == 12:
 print(a)
 """
 
+# Tab error
+# when you mix tabs and spaces
+
+a = int(input("Tell your number :- "))
+print(10/a)
+
+# Exception
+"""
+Exceptions are unexpected events or errors that occurs
+during the execution of a program, which disrupts the normal
+flow of the program
+"""
+
+"""
+print("start")
+print(10/0)            
+print("end")   This line will never execute
+"""
+
+""""
+try - wrap the block of code that might cause an exception
+
+except - Handle the exception if it occurs 
+ 
+finally - Run code no matter what, whether there's an exception or not
+
+else - Run code only if no exception occurs
+
+raise - Manually throws an exception
+
+"""
       
