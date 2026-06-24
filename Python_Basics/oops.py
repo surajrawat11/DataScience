7be4aa6a4dba7e7f5b87169382177a3120139cd1
surajrawat11 <isurajrawat23@gmail.com>
2026-06-24 17:09:57 +0530

    add ne code

---
+++ Python_Basics/oops.py
@@ -0,0 +1,9 @@
+# OBJECT ORIENTED PROGRAMMING
+
+#  A class is like a blueprint or template for creating objects.
+
+class Factory():
+    a = 12 # attribute
+
+    def hello(): # method
+        print("how are you")
