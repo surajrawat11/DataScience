2202f0a5882fbade039169fa8a14bab21a5cbcf2
surajrawat11 <isurajrawat23@gmail.com>
2026-06-25 19:53:18 +0530

    Projects

--- Python_Basics/oops.py
+++ Python_Basics/oops.py
@@ -5,5 +5,17 @@
 class Factory():
     a = 12 # attribute
 
-    def hello(): # method
+    def hello(self): # method
         print("how are you")
+
+    print("Hello how are you i am getting initialized")
+
+print(Factory().a)
+Factory().hello()
+
+# Object syntax
+obj = Factory()
+print(obj.a)
+
+# Constructor
+
